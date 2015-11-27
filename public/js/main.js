47828ea88338f0cddd12c2f6ba68a68f9a21b8f0
gustavo7lagoas <gustavo7lagoas@gmail.com>
2015-11-27 11:53:50 -0200

    adds routes and partials

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,7 @@
-angular.module('myApp', []);
+angular.module('myApp', ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider.when('/contacts', {
+            templateUrl : 'partials/contacts.html',
+            controller : 'ContactsController'
+        });
+    });
