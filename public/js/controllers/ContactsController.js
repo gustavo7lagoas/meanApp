5522b6d74c245601b49815cb95f6582b0812247c
gustavo7lagoas <gustavo7lagoas@gmail.com>
2015-11-30 16:32:28 -0200

    adds angular repeater and filter

--- public/js/controllers/ContactsController.js
+++ public/js/controllers/ContactsController.js
@@ -3,5 +3,23 @@ angular.module('myApp').controller('ContactsController',
         $scope.total = 0;
         $scope.increment = function() {
             $scope.total++;
-        }
+        };
+        $scope.contacts = [
+            {
+                _id : 1,
+                'name' : 'first contact1',
+                'email' : 'contact1@myapp.com'
+            },
+            {
+                _id : 2,
+                'name' : 'second contact2',
+                'email' : 'contact2@myapp.com'
+            },
+            {
+                _id : 3,
+                'name' : ' third contact3',
+                'email' : 'contact3@myapp.com'
+            }
+        ];
+        $scope.filter = '';
 });
