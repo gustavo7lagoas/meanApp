8b1fa57bc6a34298785f468407a40dc26ccf4040
gustavo7lagoas <gustavo7lagoas@gmail.com>
2015-12-01 11:03:58 -0200

    uses http to link back and front ends

--- public/js/controllers/ContactsController.js
+++ public/js/controllers/ContactsController.js
@@ -1,25 +1,14 @@
 angular.module('myApp').controller('ContactsController',
-   function($scope) {
-        $scope.total = 0;
-        $scope.increment = function() {
-            $scope.total++;
-        };
-        $scope.contacts = [
-            {
-                _id : 1,
-                'name' : 'first contact1',
-                'email' : 'contact1@myapp.com'
-            },
-            {
-                _id : 2,
-                'name' : 'second contact2',
-                'email' : 'contact2@myapp.com'
-            },
-            {
-                _id : 3,
-                'name' : ' third contact3',
-                'email' : 'contact3@myapp.com'
-            }
-        ];
+   function($scope, $http) {
+
+        $scope.contacts = [];
         $scope.filter = '';
+
+        $http.get('/contacts')
+        .success(function(data) {
+            $scope.contacts = data;
+        })
+        .error(function(statusText) {
+            console.log(statusText);
+        });
 });
