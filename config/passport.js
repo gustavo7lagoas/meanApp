ff5a999498213b03e45d600c21b884226f98e197
gustavo7lagoas <gustavo7lagoas@gmail.com>
2015-12-14 13:27:29 -0200

    adds authentication files

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,25 @@
 var passport = require('passport');
 var GitHubStrategy = require('passport-github').Strategy;
+var mongoose = require('mongoose');
+
+module.exports = function() {
+    passport.use(new GitHubStrategy({
+        var User = mongoose.model('User');
+        clientId : 'f8ac2a2b2603a0c2163c',
+        clientSecret : '1e8a40ac47dc130cdf653a56e031608cadae6fda',
+        callbackUrl : 'http://localhost:3000/auth/github/callback'
+    }, function(accessToken, refreshToken, profile, done) {
+        User.findOrCreate(
+            { 'login' : profile.username },
+            { 'name' : profile.username },
+            function(error, user) {
+                if(error) {
+                    console.log(error);
+                    return done(error);
+                }
+                return done(null, user);
+            }
+        );
+    }));
+};
 
